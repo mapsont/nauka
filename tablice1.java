ac4702ea0e716de346cff077053f759cb0b33e36
mapsont <mkwiatkowska93@gmail.com>
2017-02-21 13:18:11 +0100

    cheatowy sposób
    Napisz program w którym zostanie utworzona 10elementowa tablica liczb typu int. Za pomocą pętli for zapisz w kolejnych komórach liczy od 101 do 110

---
+++ tablice1.java
@@ -0,0 +1,15 @@
+public class tablice1{
+	public static void main(String args[]){
+		
+		int tablica[]=new int[10];
+		for(int i=0;i<10;i++){
+			tablica[i]= i+101;
+			}
+		
+		for(int i=0;i<10;i++){
+			System.out.println("tab["+i+"]="+tablica[i]);
+		}
+	}
+
+	
+}
